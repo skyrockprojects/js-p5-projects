9765697a4d5f055b5153b3bfb0bef8470a949ebe
WenYang-Lin <s2631502@gmail.com>
2018-03-21 16:02:59 +0800

    Update sketch.js
    put notes

--- planet_orbits/sketch.js
+++ planet_orbits/sketch.js
@@ -1,5 +1,5 @@
 const PI = 3.1415926535;
-const PI2 = PI*2;
+const PI2 = PI*2;//one lap
 
 const tickRates = [PI2/87.97, PI2/224.70, PI2/365.26, PI2/686.98, PI2/4332.82, PI2/10755.70, PI2/30687.15, PI2/60190.03];
 const distancesFromSun = [0.39, 0.723, 1, 1.524, 5.203, 9.539, 19.18, 30.06];
@@ -38,10 +38,10 @@ function drawPlanet(index){
 	var multiplier = width/(2 * distancesFromSun[mySlider.value() - 1]) - 10;
 	var a = multiplier * distancesFromSun[index];
 	var b = a * 0.7;
-	times[index] = times[index] - tickRates[index];
-	x = width/2 + a * cos(times[index]);
-	y = height/2 + b * sin(times[index]);
+	times[index] = times[index] - tickRates[index];//negative will make orbits go counterclockwise
+	x = width/2 + a * cos(times[index]);//width/2 means initial place
+	y = height/2 + b * sin(times[index]);//heighht/2 means initial place
 	fill('white');
-	var size = 4 * log(planetSizes[index])
+	var size = 4 * log(planetSizes[index]);//number is for controling the size
 	ellipse(x,y,size);
 }
